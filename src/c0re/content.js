733d7e861214950548c9ff9b8b53ae14aa6202c7
datoyacx <datoyacx@outlook.jp>
2021-08-27 23:37:34 +0700

    add updateKami and createKami

--- src/c0re/content.js
+++ src/c0re/content.js
@@ -55,4 +55,61 @@ export function getOneContent(code) {
 	});
 }
 
+export function createKami(title, description, status, text) {
+	return new Promise((resolve, reject) => {
+		const auth = localStorage.getItem("auth");
+		const formData = new FormData();
+        formData.append('title', title);
+        formData.append('description', description);
+        formData.append('status', status);
+        formData.append('type', 'kami');
+		formData.append('text', text);
+		formData.append('auth', auth);
 
+        Axios({
+            url: `${urlAPI}content/create`,
+            method: 'POST',
+            data: formData,
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'multipart/form-data',
+            },
+        }).then(response => {
+            resolve(response.data);
+            reject("fail");
+        })
+        .catch(function (error) {
+            console.error(error.response);
+        });
+	});
+}
+
+export function updateKami(title, description, status, text, code) {
+	return new Promise((resolve, reject) => {
+		const auth = localStorage.getItem("auth");
+		const formData = new FormData();
+        formData.append('title', title);
+        formData.append('description', description);
+        formData.append('status', status);
+        formData.append('type', 'kami');
+		formData.append('text', text);
+		formData.append('auth', auth);
+		formData.append('code', code);
+
+        Axios({
+            url: `${urlAPI}content/update`,
+            method: 'POST',
+            data: formData,
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'multipart/form-data',
+            },
+        }).then(response => {
+            resolve(response.data);
+            reject("fail");
+        })
+        .catch(function (error) {
+            console.error(error.response);
+        });
+	});
+}
